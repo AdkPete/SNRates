7b3618682332b4a2e066309f6fc165f3012eb36a
AdkPete <pac4607@rit.edu>
2020-09-16 10:54:46 -0400

    Added some comments, cleaned up code, added list of TODO items.

--- snrates.py
+++ snrates.py
@@ -1,7 +1,15 @@
 import numpy as np
 
+###Created by Peter Craig
+###pac4607@rit.edu
+
+
+###TODO:
+###Fix SN rates, probably via calculating them from the SFR
+###Provide a proper detection efficiency (should include effects from our program's cadence)
 class source:
 	###A class to hold useful info for each source
+	##Contains the ID, peak r band apparent magnitude (m_r) , type Ia SN rate (n1a) , core collapse SN rate (ncc)
 	def __init__(self , ID , m_r , ncc , n1a):
 		self.ID = ID
 		self.m_r = m_r
@@ -18,10 +26,12 @@ def read(): ##Reads in data file
 	##Somewhat suboptimum, but reading in two files lets us filter out sources
 	##which are outside of our sample.
 	
-	fname_1 = "peak_mags.txt"
-	fname_2 = "all_data.txt"
+	fname_1 = "peak_mags.txt" ##Data file with the same info as the google sheet
+	fname_2 = "all_data.txt" ##Data file with all relevent info (and some extra) from Yiping's Paper
 		
 	f = open(fname_1)
+	
+	##First we read in peak_mags, mostly to get a list of source id's that are in our search program.
 
 	for i in f.readlines():
 		a = i.split()
@@ -34,10 +44,11 @@ def read(): ##Reads in data file
 	
 	f = open(fname_2)
 	
-	#print (our_ids)
+	##Read through second file, gather all info for our sources
 	
 	source_list = []
 
+	##This bit is somewhat messsy, but it is functional
 	for i in f.readlines():
 		a = i.split("|")
 		if len(a) < 3:
@@ -85,9 +96,15 @@ def read(): ##Reads in data file
 			
 	
 	return source_list
-	print (len(our_ids) , len(source_list))
+	
 	
 def detection_rate(slist , detection_efficiency):
+	'''
+	Takes in a list of source objects, and a function called detection_efficiency
+	This function should take in a peak magnitude, and return a probability of detection
+	Uses the provided sn rates to estimate our detection  rate
+	'''
+	
 	
 	expected_rate = 0
 	N = 0
@@ -97,17 +114,20 @@ def detection_rate(slist , detection_efficiency):
 			
 	
 		expected_rate += detection_efficiency(i.m_r) * (i.n1a) + i.ncc * detection_efficiency(i.m_r + 2)
-	print (expected_rate)
-	print (N)
+	print ("Our expected detection rate is {} per year".format(expected_rate))
+
 	
 	
 def theoretical_de(mag):
-
-	if mag < 20.8:
+	'''
+	This is an over-simplified detection efficiency.
+	Uses a step function, returns 0 at magnitudes greater than some threshold, 1 otherwise.
+	'''
+	if mag <= 22:
 		return 1
 	return 0
 
+if __name__ == "__main__":
 	
-sl = read()
-print (len(sl))
-detection_rate(sl , theoretical_de)
+	sl = read()
+	detection_rate(sl , theoretical_de)
